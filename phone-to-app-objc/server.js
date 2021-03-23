4b18242660f5480cec9d7e96caa4c09d08306e33
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2021-03-23 13:36:19 +0000

    apps

---
+++ phone-to-app-objc/server.js
@@ -0,0 +1,41 @@
+'use strict';
+const express = require('express')
+const app = express();
+app.use(express.json());
+
+app.get('/voice/answer', (req, res) => {
+  console.log('NCCO request:');
+  console.log(`  - from: ${req.query.from}`);
+  console.log('---');
+  res.json([ 
+    { 
+      "action": "talk", 
+      "text": "Please wait while we connect you."
+    },
+    { 
+      "action": "connect", 
+      "endpoint": [ 
+        { "type": "app", "user": "Alice" } 
+      ]
+    }
+  ]);
+});
+
+app.all('/voice/event', (req, res) => {
+  console.log('EVENT:');
+  console.dir(req.body);
+  console.log('---');
+  res.sendStatus(200);
+});
+
+app.listen(3000);
+
+
+const localtunnel = require('localtunnel');
+(async () => {
+  const tunnel = await localtunnel({ 
+      subdomain: 'SUBDOMAIN', 
+      port: 3000
+    });
+  console.log(`App available at: ${tunnel.url}`);
+})();
