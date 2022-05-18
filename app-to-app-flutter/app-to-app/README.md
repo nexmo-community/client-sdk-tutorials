36c8d524cc42f2a22e6da50a17540b53271bdbad
Zachary Powell <zachary.powell@vonage.com>
2022-05-18 09:56:03 +0100

    Removed app to app, starting with app to phone

--- app-to-app-flutter/app-to-app/README.md
+++
@@ -1,3 +0,0 @@
-# app_to_app_flutter
-
-Flutter project showing how to use the Vonage voice API to create a app to app voice call
--- app-to-app-flutter/app-to-app/android/app/src/main/kotlin/com/vonage/tutorial/app_to_app_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.vonage.tutorial.app_to_app_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
