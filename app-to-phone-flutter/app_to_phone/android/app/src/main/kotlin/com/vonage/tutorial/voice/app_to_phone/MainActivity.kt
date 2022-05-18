1fbb0130738e9c744e714d279ed5b069bef84ec2
Zachary Powell <zachary.powell@vonage.com>
2022-05-18 10:00:25 +0100

    Init of flutter project

---
+++ app-to-phone-flutter/app_to_phone/android/app/src/main/kotlin/com/vonage/tutorial/voice/app_to_phone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vonage.tutorial.voice.app_to_phone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app-to-phone-flutter/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const subdomain = 'SUBDOMAIN';
+const vonageNumber = 'NUMBER';
+
+const express = require('express')
+const app = express();
+app.use(express.json());
+
+app.get('/voice/answer', (req, res) => {
+  console.log('NCCO request:');
+  console.log(`  - callee: ${req.query.to}`);
+  console.log('---');
+  res.json([ 
+    { 
+      "action": "talk", 
+      "text": "Please wait while we connect you."
+    },
+    { 
+      "action": "connect",
+      "from": vonageNumber,
+      "endpoint": [ 
+        { "type": "phone", "number": req.query.to } 
+      ]
+    }
+  ]);
+});
+
+app.all('/voice/event', (req, res) => {
+  console.log('EVENT:');
+  console.dir(req.body);
+  console.log('---');
+  res.sendStatus(200);
+});
+
+if(subdomain == "SUBDOMAIN") {
+  console.log('\n\t🚨🚨🚨 Please change the SUBDOMAIN value');
+  return false;
+}
+if(vonageNumber == "NUMBER") {
+  console.log('\n\t🚨🚨🚨 Please change the NUMBER value');
+  return false;
+}
+app.listen(3000);
+
+const localtunnel = require('localtunnel');
+(async () => {
+  const tunnel = await localtunnel({ 
+      subdomain: subdomain, 
+      port: 3000
+    });
+  console.log(`App available at: ${tunnel.url}`);
+})();
