b0225e143d736ee46e5cb6aeecb85d73d8e5dee3
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2022-11-22 14:43:48 +0000

    remove jwts

--- app-to-app-swift/AppToApp/AppToAppCall/ViewController.swift
+++ app-to-app-swift/AppToApp/AppToAppCall/ViewController.swift
@@ -86,9 +86,9 @@ struct User {
     let callPartnerName: String
     
     static let Alice = User(name: "Alice",
-                            jwt:"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njg2OTYzMzcsImp0aSI6IjgwMTk1Y2IwLTY2ODYtMTFlZC1hMjUyLTZiMThkMWQ0M2VhMSIsImFwcGxpY2F0aW9uX2lkIjoiZjY5YzZmN2MtNmU2MC00NzFjLTgxYzItYjY5MTZjODVlNTI3Iiwic3ViIjoiQWxpY2UiLCJleHAiOjE2Njg2OTYzNTk1MTQsImFjbCI6eyJwYXRocyI6eyIvKi91c2Vycy8qKiI6e30sIi8qL2NvbnZlcnNhdGlvbnMvKioiOnt9LCIvKi9zZXNzaW9ucy8qKiI6e30sIi8qL2RldmljZXMvKioiOnt9LCIvKi9pbWFnZS8qKiI6e30sIi8qL21lZGlhLyoqIjp7fSwiLyovYXBwbGljYXRpb25zLyoqIjp7fSwiLyovcHVzaC8qKiI6e30sIi8qL2tub2NraW5nLyoqIjp7fSwiLyovbGVncy8qKiI6e319fX0.cjPf_vTDbtn9Uvk1u8s88ZMDiPfJlrgkfWHmdy4TWUiweT6G9Kmh0m0xvlqUI1wQZDx0h-b__9IjfAzGHrYdldOuNu00K8KvMt3ah9UzIddC4Ojm8V_iJoKD_bpkRlrlpjjYpGGUoMtRN-owuiIQCa3wunF_C9odPmyeuoExPSfkW5jzXIDac5S08UB7MW3AqJ5pvKKc-BApQkHlnGLKemDrw649ECMWjsPWpvIS0Erq8KVKJ3H28ykl0ErjzykbsFU2lb5vgeNCKJoeSj_0Fn0aVtJq_9dH2MyXrr03UY_1rQ9ayWPidph1Ir-6G_u_n_tIyM7tPW83xBI1lTFZww",
+                            jwt:"ALICE_JWT",
                             callPartnerName: "Bob")
     static let Bob = User(name: "Bob",
-                          jwt:"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njg2OTYzNDYsImp0aSI6Ijg1NTFiYTEwLTY2ODYtMTFlZC05YjNlLTlmNjNjMmYyYmQwYyIsImFwcGxpY2F0aW9uX2lkIjoiZjY5YzZmN2MtNmU2MC00NzFjLTgxYzItYjY5MTZjODVlNTI3Iiwic3ViIjoiQm9iIiwiZXhwIjoxNjY4Njk2MzY4MjczLCJhY2wiOnsicGF0aHMiOnsiLyovdXNlcnMvKioiOnt9LCIvKi9jb252ZXJzYXRpb25zLyoqIjp7fSwiLyovc2Vzc2lvbnMvKioiOnt9LCIvKi9kZXZpY2VzLyoqIjp7fSwiLyovaW1hZ2UvKioiOnt9LCIvKi9tZWRpYS8qKiI6e30sIi8qL2FwcGxpY2F0aW9ucy8qKiI6e30sIi8qL3B1c2gvKioiOnt9LCIvKi9rbm9ja2luZy8qKiI6e30sIi8qL2xlZ3MvKioiOnt9fX19.BXLvHvdDbp03dkf6icX6zbR66o4p-iHVoNT6uxaD9Pwi_TILI39V1fQ7-y4-bxvDFwllpJ1rnge74twCUMnTP0-eWY1E6YwIFceHJBcis0KQ4bO8wwz5MAsqq9CaSiyzhb8NT8lua4bpUo5KswMfCGEcaSOsh-5Rrvm-Eh3p3VBRa2akP0PavfyVg0SywH-gTMReBHUGwJiH1hjhp2ehwjryI5A7WXEIjJ6CMnWHZX9-SwWuUUM4LFS-btXLGUXlIyzOX4Oq-OG-Zqd7Yiw5qUCPLHUJWPfHHtnYM1pUPb7HFh8BiV1FZVs1vZnOig_aq7liU5Rw4DEcUOC3LgaRDQ",
+                          jwt:"BOB_JWT",
                           callPartnerName: "Alice")
 }
